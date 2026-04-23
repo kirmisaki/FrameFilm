6c599b1712c3bb78f4705900dc2f67a858d36d15
kiritro <554359126@qq.com>
2026-04-23 15:57:03 +0800

    style(hal_epd): 格式化代码并优化代码结构
    - 统一代码缩进和花括号风格
    - 移除多余的空行和注释
    - 优化switch-case语句的格式
    - 统一循环和条件语句的格式

--- firmware/frame_film/components/film_hal/src/hal_epd.c
+++ firmware/frame_film/components/film_hal/src/hal_epd.c
@@ -81,7 +81,8 @@ static unsigned char color_get(unsigned char color_index);
 static void spi_init(void)
 {
     esp_err_t ret;
-    spi_bus_config_t buscfg = {
+    spi_bus_config_t buscfg =
+    {
         .miso_io_num = -1, // Not used
         .mosi_io_num = EPD_SDIN_PIN, // SDIN (SPI2 MOSI)
         .sclk_io_num = EPD_SCK_PIN, // SCK (SPI2 SCLK)
@@ -89,17 +90,18 @@ static void spi_init(void)
         .quadhd_io_num = -1,
         .max_transfer_sz = 0
     };
-    
+
     ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
     assert(ret == ESP_OK);
-    
-    spi_device_interface_config_t devcfg = {
+
+    spi_device_interface_config_t devcfg =
+    {
         .clock_speed_hz = 10000000, // 10MHz
         .mode = 0, // SPI mode 0
         .spics_io_num = -1, // CS pin handled manually
         .queue_size = 7
     };
-    
+
     ret = spi_bus_add_device(SPI2_HOST, &devcfg, &m_spi_device);
     assert(ret == ESP_OK);
 }
@@ -119,13 +121,13 @@ static void reset(void)
 
 static void lcd_chkstatus(void)
 {
-    while(!isEPD_W21_BUSY);                         
+    while(!isEPD_W21_BUSY);
 }
 
 static void EPD_W21_WriteCMD(unsigned char command)
 {
     EPD_W21_CS_0;
-    EPD_W21_DC_0;  // D/C#   0:command  1:data  
+    EPD_W21_DC_0;  // D/C#   0:command  1:data
     spi_transaction_t t;
     memset(&t, 0, sizeof(t));
     t.length = 8;
@@ -150,28 +152,29 @@ static unsigned char color_get(unsigned char color_index)
 {
     unsigned char datas;
 
-    switch(color_index) {
-        case 0x00: // Black
-            datas = 0x00; // 对应原始图像数据 0000
-            break;
-        case 0xff: // White
-            datas = 0x01; // 对应原始图像数据 0001
-            break;
-        case 0xfc: // Yellow
-            datas = 0x02; // 对应原始图像数据 0010
-            break;
-        case 0xE0: // Red
-            datas = 0x03; // 对应原始图像数据 0011
-            break;
-        case 0x03: // Blue
-            datas = 0x05; // 对应原始图像数据 0101
-            break;
-        case 0x1c: // Green
-            datas = 0x06; // 对应原始图像数据 0110
-            break;
-        default:
-            datas = 0x00; // 默认为黑色
-            break;
+    switch(color_index)
+    {
+    case 0x00: // Black
+        datas = 0x00; // 对应原始图像数据 0000
+        break;
+    case 0xff: // White
+        datas = 0x01; // 对应原始图像数据 0001
+        break;
+    case 0xfc: // Yellow
+        datas = 0x02; // 对应原始图像数据 0010
+        break;
+    case 0xE0: // Red
+        datas = 0x03; // 对应原始图像数据 0011
+        break;
+    case 0x03: // Blue
+        datas = 0x05; // 对应原始图像数据 0101
+        break;
+    case 0x1c: // Green
+        datas = 0x06; // 对应原始图像数据 0110
+        break;
+    default:
+        datas = 0x00; // 默认为黑色
+        break;
     }
     return datas;
 }
@@ -179,7 +182,7 @@ static unsigned char color_get(unsigned char color_index)
 void hal_epd_init(void)
 {
     gpio_config_t io_conf;
-    
+
     // Configure BUSY pin as input
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_INPUT;
@@ -187,7 +190,7 @@ void hal_epd_init(void)
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&io_conf);
-    
+
     // Configure RES, DC, CS pins as output
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
@@ -195,10 +198,10 @@ void hal_epd_init(void)
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&io_conf);
-    
+
     // Initialize SPI bus
     spi_init();
-    
+
     sys_logi(EPD_TAG, "EPD hardware initialized");
 }
 
@@ -209,7 +212,7 @@ void hal_epd_display_init(void)
     reset();
     lcd_chkstatus();
     vTaskDelay(30 / portTICK_PERIOD_MS);
-    
+
     //20211212
     EPD_W21_WriteCMD(0xAA);
     EPD_W21_WriteDATA(0x49);
@@ -221,24 +224,24 @@ void hal_epd_display_init(void)
 
     EPD_W21_WriteCMD(PWR);
     EPD_W21_WriteDATA(0x3F);
-    
+
     EPD_W21_WriteCMD(PSR);
     EPD_W21_WriteDATA(0x5F);
     EPD_W21_WriteDATA(0x69);
- 
-    
+
+
     EPD_W21_WriteCMD(BTST1);
     EPD_W21_WriteDATA(0x40);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x2C);
-    
+
     EPD_W21_WriteCMD(BTST3);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x22);
- 
+
     //===================
     //20211212
     //First setting
@@ -248,13 +251,13 @@ void hal_epd_display_init(void)
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x17);
     //===================
-    
+
     EPD_W21_WriteCMD(POFS);
     EPD_W21_WriteDATA(0x00);
     EPD_W21_WriteDATA(0x54);
     EPD_W21_WriteDATA(0x00);
     EPD_W21_WriteDATA(0x44);
-    
+
     EPD_W21_WriteCMD(TCON);
     EPD_W21_WriteDATA(0x02);
     EPD_W21_WriteDATA(0x00);
@@ -262,22 +265,22 @@ void hal_epd_display_init(void)
     EPD_W21_WriteCMD(PLL);
     EPD_W21_WriteDATA(0x08);
 
-    
+
     EPD_W21_WriteCMD(CDI);
     EPD_W21_WriteDATA(0x3F);
-    
+
     EPD_W21_WriteCMD(TRES);
     EPD_W21_WriteDATA(0x01);
     EPD_W21_WriteDATA(0x90);
-    EPD_W21_WriteDATA(0x02); 
-    EPD_W21_WriteDATA(0x58); 
-    
+    EPD_W21_WriteDATA(0x02);
+    EPD_W21_WriteDATA(0x58);
+
     EPD_W21_WriteCMD(PWS);
     EPD_W21_WriteDATA(0x2F);
- 
+
     EPD_W21_WriteCMD(T_VDCS);
     EPD_W21_WriteDATA(0x01);
-    
+
     sys_logi(EPD_TAG, "EPD display initialized");
 }
 
@@ -287,23 +290,23 @@ void hal_epd_display_white(void)
 
     EPD_W21_WriteCMD(DTM);
     {
-        for(i=0;i<120000;i++)
+        for(i = 0; i < 120000; i++)
         {
             EPD_W21_WriteDATA(EPD_COLOR_WHITE);
         }
     }
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
-    
+
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
-    
-    
+
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -319,23 +322,23 @@ void hal_epd_display_black(void)
 
     EPD_W21_WriteCMD(DTM);
     {
-        for(i=0;i<120000;i++)
+        for(i = 0; i < 120000; i++)
         {
             EPD_W21_WriteDATA(EPD_COLOR_BLACK);
         }
     }
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
-    
+
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
-    
-    
+
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -351,23 +354,23 @@ void hal_epd_display_yellow(void)
 
     EPD_W21_WriteCMD(DTM);
     {
-        for(i=0;i<120000;i++)
+        for(i = 0; i < 120000; i++)
         {
             EPD_W21_WriteDATA(EPD_COLOR_YELLOW);
         }
     }
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
-    
+
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
-    
-    
+
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -383,23 +386,23 @@ void hal_epd_display_red(void)
 
     EPD_W21_WriteCMD(DTM);
     {
-        for(i=0;i<120000;i++)
+        for(i = 0; i < 120000; i++)
         {
             EPD_W21_WriteDATA(EPD_COLOR_RED);
         }
     }
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
-    
+
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
-    
-    
+
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -415,23 +418,23 @@ void hal_epd_display_blue(void)
 
     EPD_W21_WriteCMD(DTM);
     {
-        for(i=0;i<120000;i++)
+        for(i = 0; i < 120000; i++)
         {
             EPD_W21_WriteDATA(EPD_COLOR_BLUE);
         }
     }
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
-    
+
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
-    
-    
+
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -447,23 +450,23 @@ void hal_epd_display_green(void)
 
     EPD_W21_WriteCMD(DTM);
     {
-        for(i=0;i<120000;i++)
+        for(i = 0; i < 120000; i++)
         {
             EPD_W21_WriteDATA(EPD_COLOR_GREEN);
         }
     }
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
-    
+
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
-    
-    
+
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -473,42 +476,42 @@ void hal_epd_display_green(void)
     lcd_chkstatus();
 }
 
-void hal_epd_display_pic(const unsigned char* picData)
+void hal_epd_display_pic(const unsigned char *picData)
 {
-    unsigned int i,j,k;
-    unsigned char temp1,temp2;
-    unsigned char data_H,data_L,data;
-    
-    EPD_W21_WriteCMD(0x10);        
-    for(i=0;i<EPD_HEIGHT;i++)
+    unsigned int i, j, k;
+    unsigned char temp1, temp2;
+    unsigned char data_H, data_L, data;
+
+    EPD_W21_WriteCMD(0x10);
+    for(i = 0; i < EPD_HEIGHT; i++)
     {
-        k=0;
-        for(j=0;j<EPD_WIDTH/2;j++)
+        k = 0;
+        for(j = 0; j < EPD_WIDTH / 2; j++)
         {
-            temp1=picData[i*EPD_WIDTH+k++]; 
-            temp2=picData[i*EPD_WIDTH+k++];
-            data_H=color_get(temp1)<<4;
-            data_L=color_get(temp2);
-            data=data_H|data_L;
-        
+            temp1 = picData[i * EPD_WIDTH + k++];
+            temp2 = picData[i * EPD_WIDTH + k++];
+            data_H = color_get(temp1) << 4;
+            data_L = color_get(temp2);
+            data = data_H | data_L;
+
             EPD_W21_WriteDATA(data);
         }
     }
-    
+
     //Refresh
-    
+
     EPD_W21_WriteCMD(PON);
     lcd_chkstatus();
 
     //20211212
-    //Second setting 
+    //Second setting
     EPD_W21_WriteCMD(BTST2);
     EPD_W21_WriteDATA(0x6F);
     EPD_W21_WriteDATA(0x1F);
     EPD_W21_WriteDATA(0x17);
     EPD_W21_WriteDATA(0x27);
 
-    
+
     EPD_W21_WriteCMD(DRF);
     EPD_W21_WriteDATA(0x00);
     lcd_chkstatus();
@@ -522,7 +525,7 @@ void hal_epd_sleep(void)
 {
     EPD_W21_WriteCMD(DSLP);
     EPD_W21_WriteDATA(0xA5);
-    
+
     sys_logi(EPD_TAG, "EPD entered sleep mode");
 }
 
@@ -532,6 +535,6 @@ void hal_epd_pwroff(void)
     vTaskDelay(100 / portTICK_PERIOD_MS);
 
     EPD_W21_PWR_OFF;
-    
+
     sys_logi(EPD_TAG, "EPD power off");
 }
