7d23ce5604e1a9272648f2c1afbaf61ba7342397
kirmisaki <kiritro@163.com>
2026-07-20 16:35:34 +0800

    feat(film_service): 添加网络参数相关配置与默认初始化
    1. 新增ServiceNetwork_Def_t结构体存储网络相关配置
    2. 扩展服务参数结构体加入网络配置项
    3. 添加BLE控制网络相关的指令定义
    4. 在参数默认初始化中重置网络配置参数

--- firmware/frame_film/components/film_service/inc/service_param.h
+++ firmware/frame_film/components/film_service/inc/service_param.h
@@ -26,7 +26,7 @@ typedef struct
 {
     uint32_t current_file_id;  // 当前显示的文件ID
     uint8_t load_complete;     // 加载完成标志
-    uint8_t play_mode;         // FILM模式（0：手动，1：自动）
+    uint8_t play_mode;         // FILM模式（0：手动，1：本地切换 2.网络拉取）
 } ServiceFilm_Def_t;
 
 typedef struct
@@ -36,12 +36,22 @@ typedef struct
     uint16_t sleep_time;       // 定时唤醒时间（单位分钟 10min - 48h(48*60min)）
 } ServiceSleep_Def_t;
 
+typedef struct
+{
+    uint8_t wifi_enable;       // WiFi开关 0：关闭 1：开启
+    char wifi_ssid[64];        // WiFi SSID（最大63字符）
+    char wifi_password[64];    // WiFi 密码（最大63字符）
+    char film_api_url[128];    // HTTP下载film文件的API地址
+} ServiceNetwork_Def_t;
+
+
 #pragma pack(4)
 typedef struct
 {
     uint8_t factory_flag;
     ServiceFilm_Def_t film;
     ServiceSleep_Def_t sleep;
+    ServiceNetwork_Def_t network;
 } ServiceParam_Def_t; /*服务参数*/
 #pragma pack()
 
