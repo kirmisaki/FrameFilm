3ce88c7a046100ccc585b786822fa26b698801db
kirmisaki <kiritro@163.com>
2026-07-21 10:35:32 +0800

    chore(wifi): 优化wifi初始化逻辑，避免重复初始化
    添加wifi网络接口就绪状态标记，防止重复调用esp_netif_init等初始化接口

--- firmware/frame_film/components/film_service/src/service_wifi.c
+++ firmware/frame_film/components/film_service/src/service_wifi.c
@@ -60,6 +60,7 @@
  */
 static bool g_wifi_initialized = false;
 static bool g_wifi_connected = false;
+static bool g_wifi_netif_ready = false;
 
 /*********************************************************************
  * GLOBAL VARIABLES
@@ -94,9 +95,13 @@ void service_wifi_init(void)
         return;
     }
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    if(!g_wifi_netif_ready)
+    {
+        ESP_ERROR_CHECK(esp_netif_init());
+        esp_event_loop_create_default();
+        esp_netif_create_default_wifi_sta();
+        g_wifi_netif_ready = true;
+    }
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
